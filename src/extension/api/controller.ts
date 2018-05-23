2b32dd69aeff003021107b2650932ef427ba76fc
Steffen Leistner <sleistner@gmail.com>
2018-05-23 18:06:59 +0200

    refactor: use async await

--- src/extension/api/controller.ts
+++ src/extension/api/controller.ts
@@ -30,34 +30,25 @@ export interface ICreateOptions {
 
 export class FileController {
 
-    public showMoveFileDialog(options: IMoveFileDialogOptions): Promise<FileItem> {
-
+    public async showMoveFileDialog(options: IMoveFileDialogOptions): Promise<FileItem> {
         const { prompt, showFullPath = false, uri = null } = options;
-
         const sourcePath = uri && uri.fsPath || this.sourcePath;
 
         if (!sourcePath) {
-            return Promise.reject(null);
+            throw new Error();
         }
 
         const value = showFullPath ? sourcePath : path.basename(sourcePath);
         const valueSelection = this.getFilenameSelection(value);
-
-        return Promise.resolve(window.showInputBox({ prompt, value, valueSelection }))
-            .then((targetPath) => {
-
-                if (targetPath) {
-                    targetPath = path.resolve(path.dirname(sourcePath), targetPath);
-                    return new FileItem(sourcePath, targetPath);
-                }
-
-            });
+        const targetPath = await window.showInputBox({ prompt, value, valueSelection });
+        if (targetPath) {
+            const realPath = path.resolve(path.dirname(sourcePath), targetPath);
+            return new FileItem(sourcePath, realPath);
+        }
     }
 
-    public showNewFileDialog(options: INewFileDialogOptions): Promise<FileItem> {
-
+    public async showNewFileDialog(options: INewFileDialogOptions): Promise<FileItem> {
         const { prompt, relativeToRoot = false } = options;
-
         let sourcePath = workspace.rootPath;
 
         if (!relativeToRoot && this.sourcePath) {
@@ -65,108 +56,104 @@ export class FileController {
         }
 
         if (!sourcePath) {
-            return Promise.reject(null);
+            throw new Error();
         }
 
-        return Promise.resolve(window.showInputBox({ prompt }))
-            .then((targetPath) => {
-
-                if (targetPath) {
-                    targetPath = path.resolve(sourcePath, targetPath);
-                    return new FileItem(sourcePath, targetPath);
-                }
-
-            });
+        const targetPath = await window.showInputBox({ prompt });
+        if (targetPath) {
+            const realPath = path.resolve(sourcePath, targetPath);
+            return new FileItem(sourcePath, realPath);
+        }
     }
 
-    public showRemoveFileDialog(): Promise<FileItem> {
-
+    public async showRemoveFileDialog(): Promise<FileItem> {
         const sourcePath = this.sourcePath;
 
         if (!sourcePath) {
-            return Promise.reject(null);
+            throw new Error();
         }
 
         if (!this.confirmDelete) {
-            return Promise.resolve(new FileItem(sourcePath));
+            return new FileItem(sourcePath);
         }
 
         const message = `Are you sure you want to delete '${path.basename(sourcePath)}'?`;
         const action = this.useTrash ? 'Move to Trash' : 'Delete';
-
-        return Promise.resolve(window.showInformationMessage(message, { modal: true }, action))
-            .then((remove) => remove && new FileItem(sourcePath));
+        const remove = window.showInformationMessage(message, { modal: true }, action);
+        if (remove) {
+            return new FileItem(sourcePath);
+        }
     }
 
-    public move(fileItem: FileItem): Promise<FileItem> {
-        return this.ensureWritableFile(fileItem)
-            .then(() => fileItem.move());
+    public async move(fileItem: FileItem): Promise<FileItem> {
+        const writeable = await this.ensureWritableFile(fileItem);
+        if (writeable) {
+            return fileItem.move();
+        }
     }
 
-    public duplicate(fileItem: FileItem): Promise<FileItem> {
-        return this.ensureWritableFile(fileItem)
-            .then(() => fileItem.duplicate());
+    public async duplicate(fileItem: FileItem): Promise<FileItem> {
+        const writeable = await this.ensureWritableFile(fileItem);
+        if (writeable) {
+            return fileItem.duplicate();
+        }
     }
 
-    public remove(fileItem: FileItem): Promise<FileItem> {
+    public async remove(fileItem: FileItem): Promise<FileItem> {
         return fileItem.remove(this.useTrash)
             .catch(() => Promise.reject(`Error deleting file '${fileItem.path}'.`));
     }
 
-    public create(options: ICreateOptions): Promise<FileItem> {
-
+    public async create(options: ICreateOptions): Promise<FileItem> {
         const { fileItem, isDir = false } = options;
-
-        return this.ensureWritableFile(fileItem)
-            .then(() => fileItem.create(isDir))
-            .catch(() => Promise.reject(`Error creating file '${fileItem.targetPath}'.`));
+        const writeable = await this.ensureWritableFile(fileItem);
+        if (writeable) {
+            return fileItem.create(isDir)
+                .catch(() => Promise.reject(`Error creating file '${fileItem.path}'.`));
+        }
     }
 
-    public openFileInEditor(fileItem: FileItem): Promise<TextEditor> {
-
+    public async openFileInEditor(fileItem: FileItem): Promise<TextEditor> {
         const isDir = fs.statSync(fileItem.path).isDirectory();
-
         if (isDir) {
             return;
         }
 
-        return Promise.resolve(workspace.openTextDocument(fileItem.path))
-            .then((textDocument) => {
-                return textDocument
-                    ? Promise.resolve(textDocument)
-                    : Promise.reject(new Error('Could not open file!'));
-            })
-            .then((textDocument) => window.showTextDocument(textDocument, ViewColumn.Active))
-            .then((editor) => {
-                return editor
-                    ? Promise.resolve(editor)
-                    : Promise.reject(new Error('Could not show document!'));
-            });
+        const textDocument = await workspace.openTextDocument(fileItem.path);
+        if (!textDocument) {
+            throw new Error('Could not open file!');
+        }
+
+        const editor = await window.showTextDocument(textDocument, ViewColumn.Active);
+        if (!editor) {
+            throw new Error('Could not show document!');
+        }
+
+        return editor;
     }
 
-    public closeCurrentFileEditor(): Thenable<any> {
+    public async closeCurrentFileEditor(): Promise<any> {
         return commands.executeCommand('workbench.action.closeActiveEditor');
     }
 
     private get sourcePath(): string {
-
         const activeEditor: TextEditor = window.activeTextEditor;
         const document: TextDocument = activeEditor && activeEditor.document;
 
         return document && document.fileName;
     }
 
-    private ensureWritableFile(fileItem: FileItem): Promise<FileItem> {
-
+    private async ensureWritableFile(fileItem: FileItem): Promise<FileItem> {
         if (!fileItem.exists) {
-            return Promise.resolve(fileItem);
+            return fileItem;
         }
 
         const message = `File '${fileItem.targetPath}' already exists.`;
         const action = 'Overwrite';
-
-        return Promise.resolve(window.showInformationMessage(message, { modal: true }, action))
-            .then((overwrite) => overwrite ? Promise.resolve(fileItem) : Promise.reject(null));
+        const overwrite = window.showInformationMessage(message, { modal: true }, action);
+        if (overwrite) {
+            return fileItem;
+        }
     }
 
     private getFilenameSelection(value: string): [number, number] {

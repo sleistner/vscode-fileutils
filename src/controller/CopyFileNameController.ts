0d9ec5f81c7fbd80b3e38f2083429637d4b165e2
iliashkolyar <iliashk@gmail.com>
2018-10-26 14:39:50 +0300

    Output warning to user upon clipboard errors

--- src/controller/CopyFileNameController.ts
+++ src/controller/CopyFileNameController.ts
@@ -1,11 +1,18 @@
 import { copy } from 'copy-paste';
 import * as path from 'path';
 import { promisify } from 'util';
+import { window } from 'vscode';
 import { BaseCopyController } from './BaseCopyController';
 
 const copyAsync = promisify(copy);
 
+const GENERIC_ERROR_MESSAGE = 'Could not perform copy file name to clipboard';
+
 export class CopyFileNameController extends BaseCopyController {
+    // Possible errors and their suggested solutions
+    private readonly possibleErrorsMap: { [errorMessage: string]: string} = {
+        'spawn xclip ENOENT': 'Please install xclip package (`apt-get install xclip`)'
+    };
 
     public async execute(): Promise<void> {
         const sourcePath = this.sourcePath;
@@ -15,8 +22,17 @@ export class CopyFileNameController extends BaseCopyController {
 
         const fileName = path.basename(sourcePath);
         return copyAsync(fileName)
-        .catch(() => {
-            // Can happen on unsupported platforms (e.g Linux machine without the xclip package installed)
+        .catch((error: Error) => {
+            this.handleError(error.message);
         });
+    }
+
+    private handleError(errorMessage: string): void {
+        // Can happen on unsupported platforms (e.g Linux machine without the xclip package installed).
+        // Attempting to provide a solution according to the error received
+        const errorSolution = this.possibleErrorsMap[errorMessage];
+        const warningMessageSuffix = errorSolution || errorMessage;
+
+        window.showWarningMessage(`${GENERIC_ERROR_MESSAGE}: ${warningMessageSuffix}`);
     }
 }

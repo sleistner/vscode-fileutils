6d4359a5f7bb4689b22d2ba7b3762c9b602839fb
Steffen Leistner <sleistner@gmail.com>
2023-01-23 22:45:17 +0100

    feat(NewFolder): add dedicated typeahead setting

--- src/command/NewFolderCommand.ts
+++ src/command/NewFolderCommand.ts
@@ -1,9 +1,11 @@
+import { getConfiguration } from "../lib/config";
 import { NewFileCommand } from "./NewFileCommand";
 
 export class NewFolderCommand extends NewFileCommand {
     public async execute(): Promise<void> {
+        const typeahead = this.typeahead;
         const relativeToRoot = this.options?.relativeToRoot ?? false;
-        const dialogOptions = { prompt: "Folder Name", relativeToRoot };
+        const dialogOptions = { prompt: "Folder Name", relativeToRoot, typeahead };
         const fileItems = await this.controller.showDialog(dialogOptions);
 
         if (fileItems) {
@@ -12,5 +14,9 @@ export class NewFolderCommand extends NewFileCommand {
             });
             await Promise.all(executions);
         }
+    }
+
+    protected get typeahead(): boolean {
+        return (getConfiguration("newFolder.typeahead.enabled") ?? getConfiguration("typeahead.enabled")) === true;
     }
 }

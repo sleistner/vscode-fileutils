44ac603dd241eb61e3732172ffc6cfe80555e0c0
Steffen Leistner <sleistner@gmail.com>
2023-01-23 22:47:30 +0100

    feat(DuplicateFile): add typeahead support

--- test/command/DuplicateFileCommand.test.ts
+++ test/command/DuplicateFileCommand.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
-import * as path from "path";
 import * as fs from "fs";
+import * as path from "path";
+import sinon from "sinon";
 import { Uri, window, workspace } from "vscode";
 import { DuplicateFileCommand } from "../../src/command/DuplicateFileCommand";
 import { DuplicateFileController } from "../../src/controller";
@@ -10,7 +11,10 @@ import { tmpDir } from "../helper";
 describe(DuplicateFileCommand.name, () => {
     const subject = new DuplicateFileCommand(new DuplicateFileController(helper.createExtensionContext()));
 
-    beforeEach(helper.beforeEach);
+    beforeEach(async () => {
+        await helper.beforeEach();
+        helper.createGetConfigurationStub({ "duplicateFile.typeahead.enabled": false });
+    });
 
     afterEach(helper.afterEach);
 
@@ -19,11 +23,13 @@ describe(DuplicateFileCommand.name, () => {
             beforeEach(async () => {
                 await helper.openDocument(helper.editorFile1);
                 helper.createShowInputBoxStub().resolves(helper.targetFile.path);
+                helper.createShowQuickPickStub().resolves({ label: "/", description: "" });
             });
 
             afterEach(async () => {
                 await helper.closeAllEditors();
                 helper.restoreShowInputBox();
+                helper.restoreShowQuickPick();
             });
 
             helper.protocol.it("should prompt for file destination", subject, "Duplicate As");
@@ -31,6 +37,45 @@ describe(DuplicateFileCommand.name, () => {
             helper.protocol.describe("with target file in non-existent nested directory", subject);
             helper.protocol.describe("when target destination exists", subject);
             helper.protocol.it("should open target file as active editor", subject);
+
+            describe("configuration", () => {
+                describe('when "newFile.typeahead.enabled" is "true"', () => {
+                    beforeEach(async () => {
+                        await workspace.fs.createDirectory(Uri.file(path.resolve(helper.tmpDir.fsPath, "dir-1")));
+                        await workspace.fs.createDirectory(Uri.file(path.resolve(helper.tmpDir.fsPath, "dir-2")));
+
+                        helper.createGetConfigurationStub({ "duplicateFile.typeahead.enabled": true });
+                        helper
+                            .createStubObject(workspace, "workspaceFolders")
+                            .get(() => [{ uri: Uri.file(helper.tmpDir.fsPath), name: "a", index: 0 }]);
+                    });
+
+                    it("should show the quick pick dialog", async () => {
+                        await subject.execute();
+                        expect(window.showQuickPick).to.have.been.calledOnceWith(
+                            sinon.match([
+                                { description: "- workspace root", label: "/" },
+                                { description: undefined, label: "/dir-1" },
+                                { description: undefined, label: "/dir-2" },
+                            ]),
+                            sinon.match({
+                                placeHolder: helper.quickPick.typeahead.placeHolder,
+                            })
+                        );
+                    });
+                });
+
+                describe('when "newFile.typeahead.enabled" is "false"', () => {
+                    beforeEach(async () => {
+                        helper.createGetConfigurationStub({ "duplicateFile.typeahead.enabled": false });
+                    });
+
+                    it("should not show the quick pick dialog", async () => {
+                        await subject.execute();
+                        expect(window.showQuickPick).to.have.not.been.called;
+                    });
+                });
+            });
         });
 
         helper.protocol.describe("without an open text document", subject);
@@ -65,8 +110,7 @@ describe(DuplicateFileCommand.name, () => {
             it("should prompt for file destination", async () => {
                 await subject.execute(sourceDirectory);
                 const value = sourceDirectory.path;
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                const valueSelection = [value.length - value.split(path.sep).pop()!.length, value.length];
+                const valueSelection = [value.length - (value.split(path.sep).pop() as string).length, value.length];
                 const prompt = "Duplicate As";
                 expect(window.showInputBox).to.have.been.calledWithExactly({ prompt, value, valueSelection });
             });
